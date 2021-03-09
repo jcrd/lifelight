9ac7f8dda0e80a5de59eff424a42ccd5439a3e93
James Reed <jcrd@tuta.io>
2021-03-09 10:14:47 -0700

    Implement QuadLife-style coloration

--- main.go
+++ main.go
@@ -18,8 +18,21 @@ const (
 
 const (
     CELL_DEAD int = iota
-    CELL_LIVE
+    CELL_LIVE_1
+    CELL_LIVE_2
+    CELL_LIVE_3
+    CELL_LIVE_4
+
+    CELL_N
 )
+
+var colorScheme = [CELL_N]color.Color{
+    CELL_DEAD: color.Black,
+    CELL_LIVE_1: color.RGBA{255, 0, 0, 255},
+    CELL_LIVE_2: color.RGBA{0, 255, 0, 255},
+    CELL_LIVE_3: color.RGBA{0, 0, 255, 255},
+    CELL_LIVE_4: color.White,
+}
 
 type Universe struct {
     cells [width * height]int
@@ -27,35 +40,48 @@ type Universe struct {
     matrix rgbmatrix.Matrix
     canvas *rgbmatrix.Canvas
     ticker *time.Ticker
-    color color.Color
 }
 
-func applyRules(c, n int) int {
-    if n == 3 || (n == 2 && c == CELL_LIVE) {
-        return CELL_LIVE
-    } else {
+func applyRules(c, n int, cs [CELL_N]int) int {
+    if n > 3 || n < 2 {
         return CELL_DEAD
     }
+
+    if c == CELL_DEAD && n == 3 {
+        for s, i := range cs[1:] {
+            s += 1
+            if i > 1 {
+                return s
+            }
+            if i == 0 {
+                c = s
+            }
+        }
+    }
+
+    return c
 }
 
 func (u *Universe) getIdx(x, y int) int {
     return y * width + x
 }
 
-func (u *Universe) getNeighbors(x, y int) int {
-    var count int
-
+func (u *Universe) getNeighbors(x, y int) (n int, cs [CELL_N]int) {
     for _, w := range []int{width - 1, 0, 1} {
         for _, h := range []int{height - 1, 0, 1} {
             if w == 0 && h == 0 {
                 continue
             }
             i := u.getIdx((x + w) % width, (y + h) % height)
-            count += u.cells[i]
+            cs[u.cells[i]] += 1
         }
     }
 
-    return count
+    for _, i := range cs[1:] {
+        n += i
+    }
+
+    return n, cs
 }
 
 func (u *Universe) tick() {
@@ -63,14 +89,11 @@ func (u *Universe) tick() {
 
     for x := 0; x < width; x++ {
         for y := 0; y < height; y++ {
-            var color color.Color = color.Black
             i := u.getIdx(x, y)
-            c := applyRules(u.cells[i], u.getNeighbors(x, y))
-            if c == CELL_LIVE {
-                color = u.color
-            }
+            n, cs := u.getNeighbors(x, y)
+            c := applyRules(u.cells[i], n, cs)
             u.buffer[i] = c
-            u.canvas.Set(x, y, color)
+            u.canvas.Set(x, y, colorScheme[c])
         }
     }
 
@@ -80,7 +103,11 @@ func (u *Universe) tick() {
 func (u *Universe) randomize() {
     for x := 0; x < width; x++ {
         for y := 0; y < height; y++ {
-            u.cells[u.getIdx(x, y)] = rand.Intn(2)
+            s := CELL_DEAD
+            if rand.Intn(2) == 1 {
+                s = rand.Intn(CELL_N - 1) + 1
+            }
+            u.cells[u.getIdx(x, y)] = s
         }
     }
 }
@@ -113,7 +140,6 @@ func main() {
         matrix: matrix,
         canvas: rgbmatrix.NewCanvas(matrix),
         ticker: time.NewTicker(tick),
-        color: color.White,
     }
     defer u.close()
 
